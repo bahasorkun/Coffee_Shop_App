13b24804e867781a294543da7b9c84fe517f2f3f
bahasorkun <bhsrkn@gmail.com>
2024-09-24 17:59:35 +0300

    Ana sayfa bitti

---
+++ lib/detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:coffee_shop_app/models/coffee.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key, required this.coffee});
+  final Coffee coffee;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
--- lib/homescreen_fragments/home_fragment.dart
+++ lib/homescreen_fragments/home_fragment.dart
@@ -1,9 +1,18 @@
+import 'package:coffee_shop_app/detail_page.dart';
+import 'package:coffee_shop_app/models/coffee.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:intl/intl.dart';
 
-class HomeFragment extends StatelessWidget {
+class HomeFragment extends StatefulWidget {
   const HomeFragment({super.key});
 
+  @override
+  State<HomeFragment> createState() => _HomeFragmentState();
+}
+
+class _HomeFragmentState extends State<HomeFragment> {
+  String categorySelected = "All Coffee";
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -18,19 +27,19 @@ class HomeFragment extends StatelessWidget {
                 children: [
                   const Gap(68),
                   buildHeader(),
-                  const Gap(24),
+                  const Gap(30),
                   buildSearch(),
-                  const Gap(68),
-                  // buildBannerPromo(),
+                  const Gap(24),
+                  buildBannerPromo(),
                 ],
               ),
             )
           ],
         ),
-        const Gap(20),
-        // buildCategories(),
-        const Gap(20),
-        // buildGridCoffee(),
+        const Gap(24),
+        buildCategories(),
+        const Gap(16),
+        buildGridCoffee(),
         const Gap(30),
       ],
     );
@@ -115,8 +124,8 @@ class HomeFragment extends StatelessWidget {
                       color: Colors.white,
                     ),
                     decoration: InputDecoration(
-                      contentPadding: EdgeInsets.all(0),
-                      isDense: true,
+                        contentPadding: EdgeInsets.all(0),
+                        isDense: true,
                         border: InputBorder.none,
                         hintText: "Search Coffee",
                         hintStyle: TextStyle(
@@ -147,6 +156,215 @@ class HomeFragment extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+
+  Widget buildBannerPromo() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: Image.asset(
+        "assets/icons/Banner.png",
+        width: double.infinity,
+        height: 140,
+      ),
+    );
+  }
+
+  Widget buildCategories() {
+    final categories = [
+      "All Coffee",
+      "Macchiato",
+      "Latte",
+      "Americano",
+    ];
+    return SizedBox(
+      height: 29,
+      child: ListView.builder(
+        itemCount: categories.length,
+        scrollDirection: Axis.horizontal,
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (context, index) {
+          String category = categories[index];
+          bool isActive = categorySelected == category;
+          return GestureDetector(
+            onTap: () {
+              categorySelected = category;
+              setState(() {});
+            },
+            child: Container(
+              margin: EdgeInsets.only(
+                left: index == 0 ? 24 : 8,
+                right: index == categories.length - 1 ? 24 : 8,
+              ),
+              decoration: BoxDecoration(
+                color: isActive
+                    ? const Color(0xffC67C4E)
+                    : const Color(0xffEDEDED).withOpacity(0.35),
+                borderRadius: BorderRadius.circular(6),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              alignment: Alignment.center,
+              child: Text(
+                category,
+                style: TextStyle(
+                  fontFamily: "Sora",
+                  fontSize: 14,
+                  fontWeight: isActive ? FontWeight.w600 : FontWeight.normal,
+                  color: isActive ? Colors.white : const Color(0xff313131),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildGridCoffee() {
+    return GridView.builder(
+      itemCount: listGridCoffee.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisExtent: 238,
+        crossAxisSpacing: 15,
+        mainAxisSpacing: 24,
+      ),
+      shrinkWrap: true,
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        Coffee coffee = listGridCoffee[index];
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DetailPage(coffee: coffee),
+              ),
+            );
+          },
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(8, 8, 8, 12),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        coffee.image,
+                        height: 128,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            colors: [
+                              const Color(0xff111111).withOpacity(0.3),
+                              const Color(0xff313131).withOpacity(0.3),
+                            ],
+                          ),
+                          borderRadius: const BorderRadius.only(
+                            topRight: Radius.circular(12),
+                            bottomLeft: Radius.circular(24),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Image.asset(
+                              "assets/icons/star.png",
+                              height: 12,
+                              width: 12,
+                            ),
+                            const Gap(4),
+                            Text(
+                              "${coffee.rate}",
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontFamily: "Sora",
+                                fontSize: 8,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const Gap(8),
+                Text(
+                  coffee.name,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontFamily: "Sora",
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xff242424),
+                  ),
+                ),
+                const Gap(4),
+                Text(
+                  coffee.type,
+                  style: const TextStyle(
+                    fontFamily: "Sora",
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xffA2A2A2),
+                  ),
+                ),
+                const Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      NumberFormat.currency(
+                        decimalDigits: 2,
+                        locale: "en_US",
+                        symbol: "\$ ",
+                      ).format(coffee.price),
+                      style: const TextStyle(
+                        fontFamily: "Sora",
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xff050505),
+                      ),
+                    ),
+                    Container(
+                      width: 32,
+                      height: 32,
+                      decoration: BoxDecoration(
+                        color: const Color(0xffC67C4E),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 16,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
